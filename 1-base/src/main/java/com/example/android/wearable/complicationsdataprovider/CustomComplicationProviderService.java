405589daddb407fa0cae2ce9a92e16126c63b3a6
Jeremy Walker <jewalker@google.com>
2017-05-04 18:28:17 -0700

    Revised modules/files based on new final steps.
    bug: 37917282
    Change-Id: I55eb6a0b075cd02a8294d9d0ed0f3bfca4c8f213

---
+++ 1-base/src/main/java/com/example/android/wearable/complicationsdataprovider/CustomComplicationProviderService.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.wearable.complicationsdataprovider;
+
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.SharedPreferences;
+import android.support.wearable.complications.ComplicationData;
+import android.support.wearable.complications.ComplicationManager;
+import android.support.wearable.complications.ComplicationProviderService;
+import android.support.wearable.complications.ComplicationText;
+import android.util.Log;
+
+import java.util.Locale;
+
+/**
+ * Example watch face complication data provider provides a number that can be incremented on tap.
+ */
+public class CustomComplicationProviderService extends ComplicationProviderService {
+
+    private static final String TAG = "ComplicationProvider";
+
+    /*
+     * Called when a complication has been activated. The method is for any one-time
+     * (per complication) set-up.
+     *
+     * You can continue sending data for the active complicationId until onComplicationDeactivated()
+     * is called.
+     */
+    @Override
+    public void onComplicationActivated(
+            int complicationId, int dataType, ComplicationManager complicationManager) {
+        Log.d(TAG, "onComplicationActivated(): " + complicationId);
+    }
+
+    /*
+     * Called when the complication needs updated data from your provider. There are four scenarios
+     * when this will happen:
+     *
+     *   1. An active watch face complication is changed to use this provider
+     *   2. A complication using this provider becomes active
+     *   3. The period of time you specified in the manifest has elapsed (UPDATE_PERIOD_SECONDS)
+     *   4. You triggered an update from your own class via the
+     *       ProviderUpdateRequester.requestUpdate() method.
+     */
+    @Override
+    public void onComplicationUpdate(
+            int complicationId, int dataType, ComplicationManager complicationManager) {
+        Log.d(TAG, "onComplicationUpdate() id: " + complicationId);
+
+    }
+
+    /*
+     * Called when the complication has been deactivated.
+     */
+    @Override
+    public void onComplicationDeactivated(int complicationId) {
+        Log.d(TAG, "onComplicationDeactivated(): " + complicationId);
+    }
+}
